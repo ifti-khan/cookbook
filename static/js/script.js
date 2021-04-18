02912ca26ff407004e8fe59ae1a4d45de6967d9e
Ifti Khan <ifti_k@hotmail.com>
2021-04-18 03:02:41 +0100

    mod: add recipe form added and styled but not finished

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $("#copyright-jsyear").text(new Date().getFullYear());
     $(".sidenav").sidenav({ edge: "right" });
     $('.modal').modal();
+    $('input#input_text, textarea#textarea1, textarea#textarea2').characterCounter();
+    $('select').formSelect();
 });
